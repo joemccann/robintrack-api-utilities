af6c24a7ef566c6fad7fb56c587b8961d2fe8b0a
joemccann <joseph.isaac@gmail.com>
2020-07-22 11:37:09 -0700

    added generateJson; ensure set is unique

--- src/index.js
+++ src/index.js
@@ -21,40 +21,57 @@ const downloadLatestLeaderboard = async () => {
 }
 
 const generateSymbolsFile = async (filename = 'ALL') => {
-  const { data } = await downloadLatestLeaderboard()
-  //
-  // Convert CSV string into rows
-  //
-  const rows = await csv.parse(data)
-
-  //
-  // First row is "symbol" so delete it.
-  //
-  delete rows[0]
-
-  const symbols = []
-
-  rows.forEach(row => {
-    symbols.push((row[0]).trim())
-  })
-
-  symbols.sort((a, b) => {
-    if (a > b) return 1
-    if (b > a) return -1
-    return 0
-  })
-
   try {
-    await writeFile(symbols, filename)
+    const data = await generateSymbolsJSON()
+    if (!data) throw new Error('No symbols data to write.')
+    await writeFile(data, filename)
   } catch (e) {
     console.error(e)
     return { data: null }
   }
 
-  return { data }
+  return { data: 'ok' }
+}
+
+const generateSymbolsJSON = async () => {
+  try {
+    const { data } = await downloadLatestLeaderboard()
+    //
+    // Convert CSV string into rows
+    //
+    const rows = await csv.parse(data)
+
+    //
+    // First row is "symbol" so delete it.
+    //
+    delete rows[0]
+
+    let symbols = []
+
+    rows.forEach(row => {
+      symbols.push((row[0]).trim())
+    })
+
+    symbols.sort((a, b) => {
+      if (a > b) return 1
+      if (b > a) return -1
+      return 0
+    })
+
+    //
+    // Make unique
+    //
+    symbols = [...new Set(symbols)]
+
+    return { data: symbols }
+  } catch (e) {
+    console.error(e)
+    return { data: null }
+  }
 }
 
 module.exports = {
   downloadLatestLeaderboard,
-  generateSymbolsFile
+  generateSymbolsFile,
+  generateSymbolsJSON
 }
